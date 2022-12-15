4721701db9f64a511825ae7dc968c654ec50ce83
Johnatan Chaves Ribeiro <johnatan.jcr@gmail.com>
2022-12-15 13:35:49 +0000

    Adicionado estado para capturar dados de entrada do usuário e botão para gerar a seção 'Sobre Mim' do currículo.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) =>{
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
@@ -11,10 +18,25 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Melhore seu currículo com Inteligência Artificial</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Automatize a criação da seção 'Sobre Mim' do seu currículo, adicionando informações sobre experiência profissional, educação, competências linguísticas e digitais, habilidades e projetos pessoais.</h2>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea 
+            placeholder="Experiência profissional, educação, competências linguísticas e digitais, habilidades e projetos pessoais" 
+            className="prompt-box" 
+            value={userInput}
+            onChange={onUserChangedText}
+          />;
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={null}>
+              <div className="generate">
+                <p>Generate</p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
